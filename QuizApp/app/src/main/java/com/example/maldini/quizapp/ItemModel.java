f2a01a25762aaf492da253e21705ce14f476b888
maldini58 <35968830+maldini58@users.noreply.github.com>
2018-02-10 17:24:34 +0100

    0.1.1

---
+++ QuizApp/app/src/main/java/com/example/maldini/quizapp/ItemModel.java
@@ -0,0 +1,53 @@
+package com.example.maldini.quizapp;
+
+/**
+ * Created by Maldini on 2018-02-10.
+ */
+public class ItemModel {
+
+    private String title;
+    private MainPhoto mainPhoto;
+
+    public ItemModel(String title){
+        this.title=title;
+    }
+
+    public ItemModel(String title, String mainPhoto) {
+        this.title = title;
+        this.mainPhoto = new MainPhoto(mainPhoto);
+    }
+
+    public MainPhoto getMainPhoto() {
+        return mainPhoto;
+    }
+
+    public void setMainPhoto(MainPhoto mainPhoto) {
+        this.mainPhoto = mainPhoto;
+    }
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public class MainPhoto{
+        private String url;
+
+        public MainPhoto(String url) {
+            this.url = url;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
+}
--- QuizApp/app/src/main/java/com/example/maldini/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/maldini/quizapp/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.maldini.quizapp;
 
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,29 +36,91 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-//    ProgressBar mProgressBar;
-//    private ArrayList<QuizField> mFields = new ArrayList<QuizField>();
+
     TextView textView;
+    ListView lsItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView)findViewById(R.id.textView);
+
+        // Create global configuration and initialize ImageLoader with this config
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this).build();
+        ImageLoader.getInstance().init(config);
+
+    
+
+        lsItem = (ListView)findViewById(R.id.lvItems);
+//        textView = (TextView)findViewById(R.id.textView);
         new JSONTask().execute("http://quiz.o2.pl/api/v1/quizzes/0/100");
 
 
         }
 
+//    public class ItemAdapter extends BaseAdapter {
+//        Context mContext;
+//        ArrayList<QuizField>  mQuizField;
+//        LayoutInflater mInflater;
+//        public ItemAdapter(Context c, ArrayList<QuizField> quizField) {
+//            mContext = c;
+//            mQuizField = quizField;
+//            mInflater = (LayoutInflater) mContext.getSystemService(
+//                    Context.LAYOUT_INFLATER_SERVICE);
+//
+//        }
+//        public int getCount() {
+//            return  mQuizField.size();
+//        }
+//        public Object getItem(int position) {
+//            return mQuizField.get(position);
+//        }
+//        public long getItemId(int position) {
+//            return position;
+//        }
+//        public View getView(int position, View convertView, ViewGroup parent) {
+//            ViewHolder viewHolder;
+//            if (convertView == null) {
+//                convertView = mInflater.inflate(R.layout.item_layout, null);
+//                viewHolder = new ViewHolder();
+//                viewHolder.textViewTitle = (TextView) convertView.findViewById(R.id.textViewTitle);
+//                viewHolder.imageView = (ImageView)convertView.findViewById(R.id.imageView);
+//                viewHolder.textViewResult = (TextView) convertView.findViewById(R.id.textViewResult);
+//                convertView.setTag(viewHolder);
+//            } else {
+//                viewHolder = (ViewHolder) convertView.getTag();
+//            }
+//            convertView.setBackgroundColor(Color.LTGRAY);
+//            convertView.setBackgroundColor(Color.WHITE);
+//            QuizField currentQuizField = mQuizField.get(position);
+//            viewHolder.textViewTitle.setText(currentQuizField.title);
+//            viewHolder.textViewResult.setText("Wynik");
+////            URL newurl = new URL(currentQuizField.imageUrl);
+////            mIcon_val = BitmapFactory.decodeStream(newurl.openConnection().getInputStream());
+////            profile_photo.setImageBitmap(mIcon_val);
+//
+//
+//            return convertView;
+//        }
+//    }
+//
+//
+//    private static class ViewHolder {
+//        public TextView textViewTitle;
+//        public ImageView imageView;
+//        public TextView textViewResult;
+//    }
 
 
-    public class JSONTask extends AsyncTask<String,String,String>{
+    public class JSONTask extends AsyncTask<String,String,List<ItemModel>>{
 
         @Override
-        protected String doInBackground(String... params) {
+        protected List<ItemModel> doInBackground(String... params) {
             HttpURLConnection connection = null;
             BufferedReader reader = null;
 
@@ -69,21 +144,29 @@ public class MainActivity extends AppCompatActivity {
 
                 String finalJSON = buffer.toString();
 
-//                JSONObject parentObject = new JSONObject(finalJSON);
-//                JSONArray parentArray = parentObject.getJSONArray("items");
-//                int count = parentObject.getInt("count");
-//                for(int i =0;i<count;i++) {
-//                    JSONObject finalObject = parentArray.getJSONObject(i);
-//                    JSONObject childObject = new JSONObject(finalObject.getString("mainPhoto"));
-//
-//                    String title = finalObject.getString("title");
-//                    String imageUrl = childObject.getString("url");
-//
-//                    result+= title +"\n" + imageUrl +"\n";
-//                }
-                ArrayList<QuizField> quizFields = QuizField.makeQuizFields(finalJSON);
+                JSONObject parentObject = new JSONObject(finalJSON);
+                JSONArray parentArray = parentObject.getJSONArray("items");
+                int count = parentObject.getInt("count");
 
-                return quizFields.get(0).getTitle()+"\n"+quizFields.get(0).getImageUrl();
+                List<ItemModel> itemModelList = new ArrayList<>();
+
+                for(int i =0;i<count;i++) {
+                    JSONObject finalObject = parentArray.getJSONObject(i);
+                    JSONObject childObject = new JSONObject(finalObject.getString("mainPhoto"));
+                    ItemModel itemModel = new ItemModel(finalObject.getString("title"),childObject.getString("url"));
+//                    ItemModel itemModel = new ItemModel(finalObject.getString("title"));
+
+
+                    String title = finalObject.getString("title");
+                    String imageUrl = childObject.getString("url");
+
+                    result+= itemModel.getTitle() +"\n";
+                    itemModelList.add(itemModel);
+                }
+//                ArrayList<QuizField> quizFields = QuizField.makeQuizFields(finalJSON);
+
+//                return quizFields.get(0).toString();
+                return itemModelList;
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -104,112 +187,68 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             }
-            return "Błąd";
+            return null;
         }
 
 
 
 
         @Override
-        protected void onPostExecute(String result){
+        protected void onPostExecute(List<ItemModel> result){
             super.onPostExecute(result);
-            textView.setText(result);
+//            textView.setText(result);
+            ItemAdapter itemAdapter = new ItemAdapter (getApplicationContext(),R.layout.item_layout,result);
+            lsItem.setAdapter(itemAdapter);
 
         }
     }
+
+    public class ItemAdapter extends ArrayAdapter{
+
+        private List<ItemModel> itemModelList;
+        private int resource;
+        private LayoutInflater inflater;
+        public ItemAdapter(Context context, int resource, List<ItemModel> objects) {
+            super(context, resource, objects);
+            itemModelList = objects;
+            this.resource = resource;
+            inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if(convertView == null){
+                convertView = inflater.inflate(R.layout.item_layout,null);
+            }
+            TextView textViewTitle;
+            TextView textViewResult;
+            ImageView imageView;
+
+            textViewTitle = (TextView)convertView.findViewById(R.id.textViewTitle);
+            textViewResult = (TextView)convertView.findViewById(R.id.textViewResult);
+            imageView = (ImageView)convertView.findViewById(R.id.imageView);
+
+            textViewTitle.setText(itemModelList.get(position).getTitle());
+            textViewResult.setText(itemModelList.get(position).getMainPhoto().getUrl());
+//            imageView.setImageDrawable(LoadImageFromWebOperations(itemModelList.get(position).getMainPhoto().getUrl()));
+            imageView.setImageResource(R.drawable.theon);
+
+            return convertView;
+
+        }
+
+
+    }
+
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "src name");
+            return d;
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-////        mProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
-//        if (isOnline()){
-//            LoadPhotos task = new LoadPhotos();
-//            task.execute();
-//        }else{
-////            mProgressBar.setVisibility(View.GONE);
-//            Toast.makeText(MainActivity.this.getApplicationContext(), "Aby pobierać zdjęcia, musisz się połączyć z siecią!", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//
-//
-//    public ArrayList<QuizField> getFields() {
-//        return mFields;
-//    }
-//
-//    public boolean isOnline() {
-//        return true;
-//    }
-//
-//
-//
-//    public void showList(){
-//        QuizFieldFragment photoListFragment = new QuizFieldFragment();
-//        FragmentTransaction ft = getFragmentManager().beginTransaction();
-//        ft.replace(R.id.layout_container, photoListFragment);
-//        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-//        ft.commit();
-//    }
-//
-//
-//
-//    private class LoadPhotos extends AsyncTask<String , String , Long > {
-//
-//
-//        @Override
-//        protected void onPreExecute() {
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Long result) {
-//            if (result == 0) {
-//                showList();
-//            } else {
-//                Toast.makeText(MainActivity.this.getApplicationContext(), "Coś poszło źle!", Toast.LENGTH_SHORT).show();
-//            }
-////            mProgressBar.setVisibility(View.GONE);
-//        }
-//            @Override
-//            protected Long doInBackground (String...params){
-//                HttpURLConnection connection = null;
-//                try {
-//                    URL dataUrl = new URL("http://quiz.o2.pl/api/v1/quizzes/0/100");
-//                    connection = (HttpURLConnection) dataUrl.openConnection();
-//                    connection.connect();
-//                    int status = connection.getResponseCode();
-////            Log.d("PHOTOS", status + " " + connection.getResponseMessage());
-//
-//                    if (status == 200) {
-//                        InputStream is = connection.getInputStream();
-//                        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-//                        String responseString;
-//                        StringBuilder sb = new StringBuilder();
-//                        while ((responseString = reader.readLine()) != null) {
-//                            sb = sb.append(responseString);
-//                        }
-//                        String photoData = sb.toString();
-//                        Log.d("PHOTOS", photoData);
-//                        mFields = QuizField.makeQuizFields(photoData);
-//                        return (0l);
-//                    } else {
-//                        return (1l);
-//                    }
-//                } catch (MalformedURLException e) {
-//                    e.printStackTrace();
-//                    return (1l);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                    return (1l);
-//                } catch (NullPointerException e) {
-//                    e.printStackTrace();
-//                    return (1l);
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                    return (1l);
-//                } finally {
-//                    connection.disconnect();
-//                }
-//            }
-//        }
-//    }
+
